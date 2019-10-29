fe475e7c04ca508aee9ef84c55de0673196b4b46
Bryan8x8 <bryandelgado99@yahoo.com>
2019-10-29 11:31:30 -0700

    Add files via upload

---
+++ src/coinbasedb.js
@@ -0,0 +1,43 @@
+// npm install crypto
+var crypto = require('crypto');
+// npm install request
+var request = require('request');
+
+// Set these in your ENVironment, or enter them here with the actual string
+var apiKey = '';
+var apiSecret = '';
+
+
+//get unix time in seconds
+var timestamp = Math.floor(Date.now() / 1000);
+
+// set the parameter for the request message
+var req = {
+    method: 'GET',
+    path: '/v2/exchange-rates?currency=USD',
+    body: ''
+};
+
+var message = timestamp + req.method + req.path + req.body;
+console.log(message);
+
+//create a hexedecimal encoded SHA256 signature of the message
+var signature = crypto.createHmac("sha256", apiSecret).update(message).digest("hex");
+
+//create the request options object
+var options = {
+    baseUrl: 'https://api.coinbase.com/',
+    url: req.path,
+    method: req.method,
+    headers: {
+        'CB-ACCESS-SIGN': signature,
+        'CB-ACCESS-TIMESTAMP': timestamp,
+        'CB-ACCESS-KEY': apiKey,
+        'CB-VERSION': '2015-07-22'
+    }
+};
+
+request(options,function(err, response){
+    if (err) console.log(err);
+    console.log(response.body);
+});
---
+++ src/components/Table1.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {BootstrapTable, 
+       TableHeaderColumn} from 'react-bootstrap-table';
+
+import '../../node_modules/react-bootstrap-table/css/react-bootstrap-table.css';
+import '../Table.css';
+
+function rowClassNameFormat(row, rowIdx) {
+  // row is whole row object
+  // rowIdx is index of row
+  console.log(row)
+  return row['id'] === 'Etherium' ? 
+    'GeorgeMichael-Row' : 'Other-Row';
+}
+  
+class Table1 extends Component {
+  render() {
+    return (
+      
+      <div>
+        <BootstrapTable data={this.props.data} trClassName={rowClassNameFormat}>
+          <TableHeaderColumn isKey dataField='id'>
+            Coin:
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='name' >
+            USD:
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='value'>
+            EUR:
+          </TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    );
+  }
+}
+ 
+export default Table1;
